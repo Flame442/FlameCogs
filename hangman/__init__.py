a139b893102026f27ffce3b3a95f199967f10727
Flame442 <34169552+Flame442@users.noreply.github.com>
2018-11-11 19:01:16 -0500

    Update __init__.py

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -2,6 +2,6 @@ from redbot.core import data_manager
 from .hangman import Hangman
 
 def setup(bot):
-	n = Hangman(bot)
+	n = Hangman()
 	data_manager.load_bundled_data(n, __file__)
 	bot.add_cog(n)
