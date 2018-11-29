841e665ca95862b143a81acf05657be7b933e373
Flame442 <34169552+Flame442@users.noreply.github.com>
2018-11-29 17:05:53 -0500

    Fix console spam from pms

--- deepfry/deepfry.py
+++ deepfry/deepfry.py
@@ -121,7 +121,7 @@ class Deepfry(commands.Cog):
 		
 	async def run(self, t):
 		"""Passively deepfries random images."""
-		if t.author.id != self.bot.user.id:
+		if t.author.id != self.bot.user.id and isinstance(t.channel, discord.TextChannel):
 			v = await self.config.guild(t.guild).chance()
 			if t.attachments != [] and t.attachments[0].url[::-1].split(".")[0][::-1] in ['png', 'jpg'] and t.content.find('!deepfry') == -1 and t.content.find('!nuke') == -1 and v != 0:
 				l = randint(1,v)
