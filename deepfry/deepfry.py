95af248f7b8619f4641fe1eaaf9b12ddc820d271
Flame442 <34169552+Flame442@users.noreply.github.com>
2018-12-23 13:24:32 -0500

    Fixed hard coded prefix

--- deepfry/deepfry.py
+++ deepfry/deepfry.py
@@ -125,7 +125,7 @@ class Deepfry(commands.Cog):
 		"""Passively deepfries random images."""
 		if t.author.id != self.bot.user.id and isinstance(t.channel, discord.TextChannel):
 			v = await self.config.guild(t.guild).chance()
-			if t.attachments != [] and t.attachments[0].url[::-1].split(".")[0][::-1] in ['png', 'jpg'] and t.content.find('!deepfry') == -1 and t.content.find('!nuke') == -1 and v != 0:
+			if t.attachments != [] and t.attachments[0].url[::-1].split(".")[0][::-1] in ['png', 'jpg'] and False if True in [t.content.startswith(x) for x in await self.bot.get_prefix(t)] else True and v != 0:
 				l = randint(1,v)
 				if l == 1:
 					async with aiohttp.ClientSession() as session:
