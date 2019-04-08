10291506b84f285969119303debcef51fa65a11f
Flame442 <34169552+Flame442@users.noreply.github.com>
2019-04-07 21:45:53 -0400

    Update and rename gamevoice/__init__.py to gameroles/__init__.py

---
+++ gameroles/__init__.py
@@ -0,0 +1,4 @@
+from .gameroles import GameRoles
+
+def setup(bot):
+	bot.add_cog(GameRoles(bot))
--- gamevoice/__init__.py
+++
@@ -1,6 +0,0 @@
-from .gamevoice import Gamevoice
-
-def setup(bot):
-	n = Gamevoice(bot)
-	bot.add_listener(n.update, "on_member_update")
-	bot.add_cog(n)
