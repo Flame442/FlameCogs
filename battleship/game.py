79281428390f1686baf757e511ed77d0aed61480
Flame442 <34169552+Flame442@users.noreply.github.com>
2019-09-28 15:07:16 -0400

    Adds an image board option
    Restructures the BattleshipGame params
    Config changes now update ongoing games

--- battleship/game.py
+++ battleship/game.py
@@ -1,5 +1,8 @@
 import discord
 from functools import partial
+from PIL import Image
+from redbot.core.data_manager import bundled_data_path
+from io import BytesIO
 import asyncio
 import logging
 
@@ -11,18 +14,14 @@ class BattleshipGame():
 	Params:
 	ctx = redbot.core.commands.context.Context, The context that should be used, used to send messages.
 	bot = redbot.core.bot.Red, The bot the game is running on, used to wait for messages.
-	doMention = bool, Should players be mentioned on the begining of their turn.
-	extraHit = bool, Should players get an extra shot on a hit.
+	cog = battleship.battleship.Battleship, The cog the game is running on, used to stop the game.
 	p1 = discord.member.Member, The member object of player 1.
 	p2 = discord.member.Member, The member object of player 2.
-	cog = battleship.battleship.Battleship, The cog the game is running on, used to stop the game.
 	"""
-	def __init__(self, ctx, bot, doMention, extraHit, p1, p2, cog):
+	def __init__(self, ctx, bot, cog, p1, p2):
 		self.ctx = ctx
 		self.bot = bot
 		self.cog = cog
-		self.dm = doMention
-		self.eh = extraHit
 		self.player = [p1, p2]
 		self.name = [p1.display_name, p2.display_name]
 		self.p = 1
@@ -33,6 +32,7 @@ class BattleshipGame():
 		}
 		self.pmsg = []
 		self.key = [[], []]
+		self.ship_pos = [[], []]
 		self.log = logging.getLogger('red.flamecogs.battleship')
 		self._task = asyncio.create_task(self.run())
 		self._task.add_done_callback(partial(self.error_callback, ctx)) #Thanks Sinbad <3
@@ -56,7 +56,7 @@ class BattleshipGame():
 			msg = 'Error in Battleship.\n'
 			self.log.exception(msg)
 	
-	def _bprint(self, player, show_unhit):
+	def _gen_text(self, player, show_unhit):
 		"""
 		Creates a visualization of the board.
 		Returns a str of the board.
@@ -72,6 +72,94 @@ class BattleshipGame():
 			for x in range(10): #horizontal positions
 				output += outputchars[show_unhit][self.board[player][(y*10)+x]]
 		return f'```\n{output}```'
+	
+	def _gen_img(self, player, show_unhit):
+		"""
+		Creates a visualization of the board.
+		Returns a bytes image of the board.
+		
+		Params:
+		player = int, Which player's board to print.
+		show_unhit = int, Should unhit ships be shown.
+		"""
+		path = bundled_data_path(self.cog)
+		img = Image.open(path / 'board.png')
+		hit = Image.open(path / 'hit.png')
+		miss = Image.open(path / 'miss.png')
+		ships = [
+			[
+				Image.open(path / 'len5.png'),
+				Image.open(path / 'len4.png'),
+				Image.open(path / 'len3.png'),
+				Image.open(path / 'len3.png'),
+				Image.open(path / 'len2.png')
+			], [
+				Image.open(path / 'len5destroyed.png'),
+				Image.open(path / 'len4destroyed.png'),
+				Image.open(path / 'len3destroyed.png'),
+				Image.open(path / 'len3destroyed.png'),
+				Image.open(path / 'len2destroyed.png')
+			]
+		]
+		len_key = [5, 4, 3, 3, 2]
+
+		#place ships
+		for index, pos in enumerate(self.ship_pos[player]):
+			x, y, d = pos
+			length = len_key[index]
+			if show_unhit and not all(self.key[player][index].values()): #show a non-damaged ship
+				if d == 'd': #down
+					ships[0][index] = ships[0][index].rotate(90, expand=True)
+				img.paste(ships[0][index], box=((x*30)+32, (y*30)+32), mask=ships[0][index])
+			elif all(self.key[player][index].values()): #show a damaged ship
+				if d == 'd': #down
+					ships[1][index] = ships[1][index].rotate(90, expand=True)
+				img.paste(ships[1][index], box=((x*30)+32, (y*30)+32), mask=ships[1][index])
+		
+		#place hit/miss markers	
+		for y in range(10):
+			for x in range(10):
+				if self.board[player][((y)*10)+x] == 1: #miss
+					img.paste(miss, box=((x*30)+32, (y*30)+32), mask=miss)
+				elif self.board[player][((y)*10)+x] == 2: #hit
+					img.paste(hit, box=((x*30)+32, (y*30)+32), mask=hit)
+			
+		temp = BytesIO()
+		temp.name = 'board.png'
+		img.save(temp)
+		temp.seek(0)
+		return temp
+	
+	async def update_dm(self, player):
+		"""
+		Update the DM board for a specific player.
+		Only updates the board if the board is not an image.
+		
+		Params:
+		player = int, Which player's board to print.
+		"""
+		if not await self.cog.config.guild(self.ctx.guild).doImage():
+			content = self._gen_text(player, 1)
+			await self.pmsg[player].edit(content=content)
+	
+	async def send_board(self, player, show_unhit, dest, msg):
+		"""
+		Send either an image of the board or a text representation of the board.
+		
+		player = int, Which player's board to print.
+		show_unhit = int, Should unhit ships be shown.
+		dest = discord.abc.Messageable, Where to send to.
+		msg = str, Text to include with the board.
+		"""
+		if await self.cog.config.guild(self.ctx.guild).doImage():
+			img = self._gen_img(player, show_unhit)
+			file = discord.File(img, 'board.png')
+			m = await dest.send(file=file)
+			if msg:
+				m = await dest.send(msg)
+			return m
+		content = self._gen_text(player, show_unhit)
+		return await dest.send(f'{content}{msg}')
 	
 	async def _place(self, player, length, value):
 		"""
@@ -133,6 +221,7 @@ class BattleshipGame():
 			await self.player[player].send('Invalid input, too far down.')
 			return False
 		self.key[player].append(hold)
+		self.ship_pos[player].append((x, y, d))
 		return True
 	
 	async def run(self):
@@ -148,9 +237,7 @@ class BattleshipGame():
 				'and the direction of (r)ight or (d)own in xyd format.'
 			)
 			for ship_len in [5, 4, 3, 3, 2]: #each ship length
-				await self.player[x].send(
-					f'{self._bprint(x,1)}Place your {ship_len} length ship.'
-				)
+				await self.send_board(x, 1, self.player[x], f'Place your {ship_len} length ship.')
 				while True:
 					try:
 						t = await self.bot.wait_for(
@@ -166,21 +253,20 @@ class BattleshipGame():
 						return self.stop()
 					if await self._place(x, ship_len, t.content.lower()): #only break if _place succeeded
 						break
-			m = await self.player[x].send(self._bprint(x, 1))
+			m = await self.send_board(x, 1, self.player[x], '')
 			self.pmsg.append(m) #save this message for editing later
 		game = True
 		pswap = {1:0, 0:1} #helper to swap player
 		channel = self.ctx.channel
 		while game:
 			self.p = pswap[self.p] #swap players
-			if self.dm: #should player be mentioned
+			if await self.cog.config.guild(self.ctx.guild).doMention(): #should player be mentioned
 				mention = self.player[self.p].mention
 			else:
 				mention = self.name[self.p]
-			await self.ctx.send(
-				f'{mention}\'s turn!\n'
-				f'{self._bprint(pswap[self.p], 0)}'
-				f'{self.name[self.p]}, take your shot.'
+			await self.ctx.send(f'{mention}\'s turn!')
+			await self.send_board(
+				pswap[self.p], 0, self.ctx, f'{self.name[self.p]}, take your shot.'
 			)
 			while True:
 				try:
@@ -203,35 +289,35 @@ class BattleshipGame():
 					continue
 				if self.board[pswap[self.p]][(y*10)+x] == 0:
 					self.board[pswap[self.p]][(y*10)+x] = 1
-					await self.pmsg[pswap[self.p]].edit(content=self._bprint(pswap[self.p], 1))
-					await self.ctx.send(f'{self._bprint(pswap[self.p], 0)}**Miss!**')
+					await self.update_dm(pswap[self.p])
+					await self.send_board(pswap[self.p], 0, self.ctx, '**Miss!**')
 					break
 				elif self.board[pswap[self.p]][(y*10)+x] in [1, 2]:
 					await self.ctx.send('You already shot there!')
 				elif self.board[pswap[self.p]][(y*10)+x] == 3:
 					self.board[pswap[self.p]][(y*10)+x] = 2
-					await self.pmsg[pswap[self.p]].edit(content=self._bprint(pswap[self.p], 1))
-					await self.ctx.send(f'{self._bprint(pswap[self.p], 0)}**Hit!**')
 					#DEAD SHIP
+					ship_dead = None
 					for a in range(5):
 						if (y*10)+x in self.key[pswap[self.p]][a]:
 							self.key[pswap[self.p]][a][(y*10)+x] = 1
-							any_unhit = False
-							for b in self.key[pswap[self.p]][a]:
-								if self.key[pswap[self.p]][a][b] == 0: #if any position in the ship is still there
-									any_unhit = True
-									break
-							if not any_unhit: #if ship destroyed
-								await self.ctx.send(
-									f'**{self.name[pswap[self.p]]}\'s {[5, 4, 3, 3, 2][a]} '
-									'length ship was destroyed!**'
-								)
+							if all(self.key[pswap[self.p]][a].values()): #if ship destroyed
+								ship_dead = [5, 4, 3, 3, 2][a]
+					await self.update_dm(pswap[self.p])
+					if ship_dead:
+						msg = (
+							f'**Hit!**\n**{self.name[pswap[self.p]]}\'s '
+							f'{ship_dead} length ship was destroyed!**'
+						)
+						await self.send_board(pswap[self.p], 0, self.ctx, msg)
+					else:
+						await self.send_board(pswap[self.p], 0, self.ctx, '**Hit!**')
 					#DEAD PLAYER
 					if 3 not in self.board[pswap[self.p]]:
 						await self.ctx.send(f'**{self.name[self.p]} wins!**')
 						game = False
 					if game:
-						if self.eh:
+						if await self.cog.config.guild(self.ctx.guild).extraHit():
 							await self.ctx.send('Take another shot.')
 						else:
 							break
