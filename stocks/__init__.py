fef752cc1de58d5c4e1831f92b5dddcacaef8211
Flame442 <34169552+Flame442@users.noreply.github.com>
2020-08-19 17:40:42 -0400

    Update __init__.py

--- stocks/__init__.py
+++ stocks/__init__.py
@@ -1,4 +1,6 @@
 from .stocks import Stocks
 
+__red_end_user_data_statement__ = 'This cog stores a user id in order to know what stocks have been purchased by each user. Data is only collected when a user directly interacts with the cog and its commands.'
+
 def setup(bot):
 	bot.add_cog(Stocks(bot))
