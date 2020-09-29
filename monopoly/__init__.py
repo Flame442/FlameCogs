d733003660efd9edb9d347ff9f58bfc2e9599374
Flame442 <34169552+Flame442@users.noreply.github.com>
2020-09-28 22:33:00 -0400

    Update __init__.py

--- monopoly/__init__.py
+++ monopoly/__init__.py
@@ -1,4 +1,6 @@
 from .monopoly import Monopoly
 
+__red_end_user_data_statement__ = 'This cog stores user ids in order to identify the players in saved games.'
+
 def setup(bot):
 	bot.add_cog(Monopoly(bot))
