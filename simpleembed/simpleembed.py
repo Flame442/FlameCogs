e3a8d313c301e64389218e5759a3ac3d03dd576d
Wyn <841141+TheWyn@users.noreply.github.com>
2020-11-07 23:18:25 -0500

    Removed attachments for urls
    It's much easier with url's when using it through another cog (Trusty's retrigger for one)

--- simpleembed/simpleembed.py
+++ simpleembed/simpleembed.py
@@ -5,37 +5,39 @@ from typing import Optional
 
 
 class SimpleEmbed(commands.Cog):
-	"""Simply send embeds."""
-	def __init__(self, bot):
-		self.bot = bot
-	
-	@checks.has_permissions(manage_messages=True)
-	@commands.bot_has_permissions(embed_links=True)
-	@commands.command()
-	async def sendembed(self, ctx, color: Optional[discord.Color]=None, *, text):
-		"""
-		Send an embed.
-		
-		Use the optional parameter `color` to change the color of the embed.
-		The embed will contain the text `text`.
-		All normal discord formatting will work inside the embed.
-		Send the optional image with the command to insert an image at the bottom of the embed.
-		"""
-		if color is None:
-			color = await ctx.embed_color()
-		embed = discord.Embed(
-			description=text,
-			color=color
-		)
-		if ctx.message.attachments:
-			content = await ctx.message.attachments[0].to_file()
-			embed.set_image(url="attachment://" + str(content.filename))
-		await ctx.send(embed=embed, file=content if ctx.message.attachments else None)
-		try:
-			await ctx.message.delete()
-		except discord.Forbidden:
-			pass
+    """Simply send embeds."""
 
-	async def red_delete_data_for_user(self, **kwargs):
-		"""Nothing to delete."""
-		return
+    def __init__(self, bot):
+        self.bot = bot
+
+    @checks.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(embed_links=True)
+    @commands.command()
+    async def sendembed(self, ctx, color: Optional[discord.Color] = None, image=None, thumbnail=None, *, text):
+        """
+        Send an embed.
+
+        Use the optional parameter `color` to change the color of the embed.
+        Use the optional parameter 'image' to include an image url
+        Use the optional parameter 'thumbnail' to include an thumbnail image url
+        The embed will contain the text `text`.
+
+        """
+        if color is None:
+            color = await ctx.embed_color()
+        embed = discord.Embed(
+            description=text,
+            color=color
+        )
+
+        if image is not None:
+            embed.set_image(url=image)
+
+        if thumbnail is not None:
+            embed.set_thumbnail(url=thumbnail)
+
+        await ctx.send(embed=embed)
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete."""
+        return
